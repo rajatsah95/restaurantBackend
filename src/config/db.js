408ddc38789366a4ab415b9e042a00cec99208a2
rajatsah95 <rajatsah95@gmail.com>
2025-12-11 18:24:59 +0530

    restaurantBackend added 5

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,6 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const pool = mysql.createPool({
-  host: "127.0.0.1",
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  port: 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+const pool = mysql.createPool("mysql://root:syMQAdPCTjigbayuFYxmLaNpwrtxllET@shuttle.proxy.rlwy.net:46385/railway");
 
 module.exports = pool;
